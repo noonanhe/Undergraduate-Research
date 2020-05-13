0ee6282fb9ecbaeea32e07e8f5f1cc1aaecee788
Heather Noonan <54756502+noonanhe@users.noreply.github.com>
2020-05-13 11:30:32 -0400

    Update concat_fasta.cpp
    Added better comments

--- UURAF 2019 Project/concat_fasta.cpp
+++ UURAF 2019 Project/concat_fasta.cpp
@@ -11,31 +11,34 @@ using std::flush;
 #include <sstream>
 using std::ostringstream;
 #include <iterator>
-/*need to open file
-maybe i should use a map with key as their ID tag and 
-find the tags i need and then add that block to the corresponding file
-*/
+
 
 map <string, string> mice= {{"ASS",""},{"AMQ",""},{"ASA",""},{"AKS",""},{"ARI",""},
     {"ALK",""},{"AWQ",""},{"ADY",""},{"AEM",""},{"AMG",""},{"ANA",""},{"AOI",""},
     {"AIG",""},{"ACS",""},{"ADQ",""},{"ALW",""},{"AMO",""},{"ALQ",""},{"AJS",""},
     {"AZC",""},{"ACQ",""},{"BAG",""},{"AKI",""},{"APO",""},{"AQC",""},{"AWK",""},
     {"AXE",""},{"ABY",""}}; 
-//ifstream in_file("test_fasta.txt");
+
 map <string, string> species= {{"ADG",""},{"ASS",""},{"ARU",""},{"AXQ",""},{"AIE",""},{"AWI",""},{"AGK",""}
    ,{"AIK",""},{"AGM",""},{"AFS",""},{"AZQ",""},{"AOQ",""},{"AIO",""},{"AHU",""},{"AMQ",""},{"ASA",""},{"AKS",""},
    {"AGA",""},{"AXC",""},{"AQY",""},{"AFM",""},{"ALO",""},{"AAQ",""},{"APA",""},{"AIL",""},{"AJE",""},{"AQS",""},
    {"ASQ",""},{"ATK",""}};
-string test= "AAAAAAAAAABFG";
-string test2= "ACTGACTGATG";
 
+/** Determines whether a string is an identifier or not
+* \param str Line from a file being read in
+* \returns bool indicating whether the line contains the identifier/key
+**/
 bool is_key(string& str){
     if (str.size()==11){
         return true;
-    }//
+    }
     return false;
-}//checks to see if line is an identifier
+}
 
+/**Cleans up the line containing an identifier to just the identifier
+* \param str Line containing identifier
+* \returns cleaned identifier
+**/
 string make_key(string& str){
     string key;
     for (auto i=str.size()-3; i<str.size(); ++i){
@@ -44,6 +47,11 @@ string make_key(string& str){
     return key;
 }//make_key
 
+/** Prints the elements in a map
+* \param out The out stream to use
+* \param m The map to print out
+* \returns the outstream being printed to
+**/
 ostream& print_map(ostream& out, map<string,string> &m){
     for (auto element: m){
         out << element.first << ":" <<element.second<<endl;
@@ -51,6 +59,9 @@ ostream& print_map(ostream& out, map<string,string> &m){
     return out;
 }//print_map
 
+/** Reads in the file contain the mouse DNA data
+* \param name The name of the file containing the data
+**/
 void read_file(string name){
     string str="";
     string key;
@@ -63,12 +74,6 @@ void read_file(string name){
                 itr=species.find(key);
                 (*itr).second+=sequence;
             }//if a key is in the map
-            /*else{
-                itr= (mice.insert({key,""})).first;
-            }*/
-            /*(*itr).second+=sequence;
-            key="";
-            sequence.clear();*/
             key="";
             sequence.clear();
         }//if its a line break
@@ -81,16 +86,9 @@ void read_file(string name){
             }//if its a sequence line
         }//if its not a line break
     }//while there is still lines in the file
-    /*if (mice.find(key)!=mice.end()){
-        itr=mice.find(key);
-    }
-    else {
-        itr= (mice.insert({key,""})).first;
-    }
-        sequence+=str.substr(str.size()-1);
-        (*itr).second+=sequence;*/
 }//read_file function
 
+/// Writes the desired data to a file in phylip format
 void write_file_phylip(){
     ofstream out_file;
     out_file.open("outfile_species_phylip.txt");
@@ -101,6 +99,7 @@ void write_file_phylip(){
     }//for each string in the vector
 }//writes to an output file
 
+/// Writes the desired data to a file in fasta format
 void write_file_fasta(){
     ofstream out_file;
     out_file.open("outfile_species_fasta.txt");
@@ -111,6 +110,8 @@ void write_file_fasta(){
 }//write to an output file but in fasta format
 
 int main(){
+    /** The DNA sequences of each mouse were separated into 20 different files, because their DNA was sequences in 20 different chunks.
+    I chose to read in each of these 20 files separately, concatanating them within this formatting script. **/
     read_file("phased.fasta");
     read_file("phased.fasta2");
     read_file("phased.fasta3");
@@ -123,13 +124,14 @@ int main(){
     read_file("phased.fasta10");
     read_file("phased.fasta11");
     read_file("phased.fasta12");
-    read_file("phased.fasta13");read_file("phased.fasta14");
-    read_file("phased.fasta15");read_file("phased.fasta16");
+    read_file("phased.fasta13");
+    read_file("phased.fasta14");
+    read_file("phased.fasta15");
+    read_file("phased.fasta16");
     read_file("phased.fasta17");
     read_file("phased.fasta18");
     read_file("phased.fasta19");
     read_file("phased.fastaX");
-    //print_map(cout, mice);
     for (auto element: species){
         cout << element.first <<" "<<element.second.size()<<endl;
     }
